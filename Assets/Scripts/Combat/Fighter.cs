7b5f657441012260d0b97e41ae7f91b1c09744a4
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-04-23 17:49:27 -0300

    Creating Scheduler

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Movement;
+using RPG.Core;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -31,6 +32,7 @@ namespace RPG.Combat
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
         }
 
