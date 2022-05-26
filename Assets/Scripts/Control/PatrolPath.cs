eb8b7276a92382928cf09dfc9e7d5251ea01946e
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-05-25 21:20:20 -0300

    Path Loops

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Control
@@ -7,12 +5,29 @@ namespace RPG.Control
     public class PatrolPath : MonoBehaviour
     {
         const float waypointGizmosRadius = 0.3f;
+
         private void OnDrawGizmos() 
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmosRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmosRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 >= transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
