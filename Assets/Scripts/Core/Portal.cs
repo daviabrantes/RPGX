9d97a4ad2f740fecc09cb31edb136f2b48e16d17
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-06-30 20:32:19 -0300

    Spawn Place

--- Assets/Scripts/Core/Portal.cs
+++ Assets/Scripts/Core/Portal.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other) 
         {
@@ -20,6 +23,25 @@ namespace RPG.SceneManagement
         private IEnumerator Transition()
         {
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayerPosition(otherPortal);
+        }
+        
+        private void UpdatePlayerPosition(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+            return null;
         }
     }
 }
