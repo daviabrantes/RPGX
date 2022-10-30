cabe27d1739dae3b8ae158a9fe66edeb12a9e8d5
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-10-30 01:18:30 -0300

    Player Controller Fix

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -60,7 +60,7 @@ namespace RPG.Control
 
         private static Ray GetMouseRay()
         {
-            return Camera.current.ScreenPointToRay(Input.mousePosition);
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
