fae83991cf52398139d29373f4e51f26abc12044
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-11-17 15:01:51 -0300

    Arrow Damage

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,6 +6,7 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1;
     Health target = null;
+    float damage = 0;
 
     void Update()
     {
@@ -15,9 +16,10 @@ public class Projectile : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    public void SetTarget(Health target)
+    public void SetTarget(Health target, float damage)
     {
         this.target = target;
+        this.damage = damage;
     }
 
     private Vector3 GetAimLocation()
@@ -27,5 +29,12 @@ public class Projectile : MonoBehaviour
         if (targetCapsule == null) return target.transform.position;
 
         return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != target) return;
+        target.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -43,7 +43,7 @@ namespace RPG.Combat
         public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target)
         {
             Projectile projectileInstance = Instantiate(projectile, GetTransformHand(rightHand, leftHand).position, Quaternion.identity);
-            projectileInstance.SetTarget(target);
+            projectileInstance.SetTarget(target, weaponDamage);
         }
 
         public bool HasProjectile()
