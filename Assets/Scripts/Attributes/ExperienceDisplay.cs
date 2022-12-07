34407800e2f3f04b09ce6da7511d6c03c076324f
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-12-07 15:24:53 -0300

    Experience Points HUD

---
+++ Assets/Scripts/Attributes/ExperienceDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Attributes
+{
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        Experience experience;
+
+        private void Awake()
+        {
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+        }
+
+        private void Update()
+        {
+            GetComponent<Text>().text = experience.GetPoints().ToString();
+        }
+    }
+}
