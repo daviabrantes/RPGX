3b65e2bf43cb059a1a6170c9d7636c5d5e51dd8d
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-12-11 16:31:07 -0300

    Fix Saving

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -38,6 +38,7 @@ namespace RPG.Saving
         public void Delete(string saveFile)
         {
             File.Delete(GetPathFromSaveFile(saveFile));
+            print("Deleting Save");
         }
 
         private Dictionary<string, object> LoadFile(string saveFile)
