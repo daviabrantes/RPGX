1d15d2385d740504443ceb222a75252975c12221
Davi e Tal <davi.souza.abrantes@gmail.com>
2023-01-13 11:33:37 -0300

    Fader fix

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -115,7 +115,7 @@ namespace RPG.Attributes
 
         public object CaptureState()
         {
-            return healthPoints;
+            return healthPoints.value;
         }
 
         public void RestoreState(object state)
